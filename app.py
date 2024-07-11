d34601c94085c105ab0efe936029a8cd119b51ce
Rodrigo Cap <ingkapa@yahoo.com.ar>
2024-07-11 11:00:33 -0300

    Agrego funcion targets

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ df_optimo = funciones.tratar_df(df)
 
 ##Standar scaler para floats - Por ahora no lo hago
 
+############   Agrego columnas target   #################
+df_target = funciones.crea_target_long(df=df_optimo, nombre_target="LONG_target_20p_05",porciento_varia = 0.5, proyeccion = 20)
+print(df_target["target"])
 
 ##################### Train y test del df original   ###################
 # Defino X e y
@@ -30,54 +33,24 @@ ros = RandomOverSampler(random_state=49150)
 X_resampled, y_resampled = ros.fit_resample(X, y)
 
 
-###################   Entrenamiento   #########################
+################### Separo en entrenamiento y testing  ###############################
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.25, random_state=49150)
 
-search_space1 = {
-    'num_iterations': Integer(1500, 3000),
-    'learning_rate': Real(0.1, 0.4),
-    'max_depth': Integer(80, 150),
-    #'objective': 'binary',
-    'num_leaves': Integer(20, 50),
-    'min_data_in_leaf': Integer(1, 15),
-    'max_bin': Integer(20, 50),
-    'bagging_fraction': Real(0.5 , 1),
-    'feature_fraction':Real(0.5, 1),
-    #'categorical_feature':
-}
-
-# Instanciar BayesSearchCV con el modelo y el espacio de búsqueda
-lgbm = LGBMClassifier()
-lgbm_bayes_search = BayesSearchCV(lgbm, search_space1, n_iter=70, scoring='f1', cv=5, random_state=49150, n_jobs=-1)
-
-# Ajustar BayesSearchCV al conjunto de entrenamiento
-lgbm_bayes_search.fit(X_train, y_train)
-
-# Imprimir mejores parametros
-print(f'Best Parameters: {lgbm_bayes_search.best_params_}')
+###################   Encontrar los mejores parametros   ######################### deberia ser una funcion
+#mejor_modelo, mejores_hiperparametros = funciones.buscar_hiper_LGBM(X_resampled, y_resampled)
 
 ################## Metricas #########################
-#Predigo con los nuevos hiperparametros
-y_pred_lgbm = lgbm_bayes_search.predict(X_test)
-
-# Calcular métricas
-accuracy_lgbm = accuracy_score(y_test, y_pred_lgbm)
-cm_lgbm = confusion_matrix(y_test, y_pred_lgbm)
-precision_lgbm = precision_score(y_test, y_pred_lgbm)#, average='weighted')
-recall_lgbm = recall_score(y_test, y_pred_lgbm)#, average='weighted')
-f1_lgbm = f1_score(y_test, y_pred_lgbm)#, average='weighted')
+#accuracy, precision, recall, f1, cm = funciones.calcular_metricas(X_test, y_test, mejor_modelo)
 
 # Imprimir métricas
-print('Accuracy: ', accuracy_lgbm)
-print('Precision: ', precision_lgbm)
-print('Recall: ', recall_lgbm)
-print('F1 Score: ', f1_lgbm)
-print('Matriz de confusion: ', cm_lgbm)
-
+'''print('Accuracy: ', accuracy)
+print('Precision: ', precision)
+print('Recall: ', recall)
+print('F1 Score: ', f1)
+print('Matriz de confusion: ',cm)'''
 
 ################### Guardo Modelo ##################### *****NUEVO******
-funciones.guardar_modelo(lgbm)#Agregar el parametro "Nuevo Nombre"
-
+#funciones.guardar_modelo(mejor_modelo)#Agregar el parametro "Nuevo Nombre"
 
 # Configurar el URI de seguimiento de MLflow para usar Google Drive
-mlflow.set_tracking_uri('/mlruns')
+#mlflow.set_tracking_uri('/mlruns')
--- funciones.py
+++ funciones.py
@@ -10,11 +10,13 @@ def categorizar_df(df):
   df_optimo = df.replace(mapping).copy()
   return df_optimo
 
+#Funcion que normaliza los valores del tipo float mediante un standard scaler
 def normalizar_floats(df):
   scaler = StandardScaler() # (x-M)/d
   df[col_indicadores_float] = scaler.fit_transform(df[col_indicadores_float]).copy()#.drop(df_optimizado[['close']],axis = 1))
   return df
 
+#Funcion
 def tratar_df(df):
     #Transformo las variables categoricas
     df_optimo = categorizar_df(df)
@@ -23,17 +25,88 @@ def tratar_df(df):
     df_optimo.rename(columns={'RSI[1]':'RSI1', 'Stoch.K[1]':'Stoch.K1', 'Stoch.D[1]': 'Stoch.D1','CCI20[1]':'CCI201', 
     'ADX+DI[1]':'ADX+DI1', 'ADX-DI[1]':'ADX-DI1', 'AO[1]':'AO1', 'Mom[1]':'Mom1', 'AO[2]':'AO2'},inplace = True)
 
-    df_optimo['tomorrow'] = df_optimo["close"].shift(-1)
-    df_optimo['target'] = (df_optimo['tomorrow'] > df_optimo["close"]).astype(int)
-
     return df_optimo
 
 
 #Funcion que guarda el modelo ****NUEVO****
 def guardar_modelo(modelo):
-   file_path = '/modelos'
+  file_path = '/modelos'
 
-   # Llamar a la función de guardar después de entrenar el modelo
-   ruta_modelo = os.path.join(file_path, 'lgbm_tuning_model.joblib')
-   dump(modelo, ruta_modelo)
-   print(f"Modelo guardado en: {ruta_modelo}")
+  # Llamar a la función de guardar después de entrenar el modelo
+  ruta_modelo = os.path.join(file_path, 'lgbm_tuning_model.joblib')
+  dump(modelo, ruta_modelo)
+  print(f"Modelo guardado en: {ruta_modelo}")
+
+
+### Funcion que encuentra los mejores hiperparametros para un modelo LGBM
+def buscar_hiper_LGBM (X,y):
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=49150)
+
+  search_space1 = {
+      'num_iterations': Integer(1500, 3000),
+      'learning_rate': Real(0.1, 0.4),
+      'max_depth': Integer(80, 150),
+      #'objective': 'binary',
+      'num_leaves': Integer(20, 50),
+      'min_data_in_leaf': Integer(1, 15),
+      'max_bin': Integer(20, 50),
+      'bagging_fraction': Real(0.5 , 1),
+      'feature_fraction':Real(0.5, 1),
+      #'categorical_feature':
+  }
+
+  # Instanciar BayesSearchCV con el modelo y el espacio de búsqueda
+  lgbm = LGBMClassifier()
+  lgbm_bayes_search = BayesSearchCV(lgbm, search_space1, n_iter=1, scoring='f1', cv=5, random_state=49150, n_jobs=-1)
+
+  # Ajustar BayesSearchCV al conjunto de entrenamiento
+  lgbm_bayes_search.fit(X_train, y_train)
+
+  # Imprimir mejores parametros
+  print(f'Best Parameters: {lgbm_bayes_search.best_params_}')
+
+  return lgbm_bayes_search, lgbm_bayes_search.best_params_
+
+
+
+### Funcion que calcula las metricas
+def calcular_metricas(X, y, mejor_modelo):
+  #Predigo con los nuevos hiperparametros
+  y_pred_lgbm = mejor_modelo.predict(X)
+
+  # Calcular métricas
+  accuracy_lgbm = accuracy_score(y, y_pred_lgbm)
+  cm_lgbm = confusion_matrix(y, y_pred_lgbm)
+  precision_lgbm = precision_score(y, y_pred_lgbm)#, average='weighted')
+  recall_lgbm = recall_score(y, y_pred_lgbm)#, average='weighted')
+  f1_lgbm = f1_score(y, y_pred_lgbm)#, average='weighted') 
+
+  return accuracy_lgbm, cm_lgbm, precision_lgbm, recall_lgbm, f1_lgbm
+
+
+
+### Funcion: Si el precio toca el objetivo POSITIVO en el periodo elegido (proyeccion) devuelve 1. Si toca el stop o no toca el target devuelve 0
+def toca_long(referencia, porciento_varia, columna, proyeccion):
+
+  variacion_positiva = 1 + (porciento_varia/100)
+  variacion_negativa = 1 - (porciento_varia/100)
+  resultado = 0
+
+  # Recorremos la columna 'close' y aplicamos las condiciones
+  for precio in columna.head(proyeccion):
+    if precio >= variacion_positiva * referencia:
+      resultado = 1
+    elif precio <= variacion_negativa * referencia:
+      resultado = 0
+  return resultado
+
+### Funcion que crea una columna  con 1 si se toco el target positivo y 0 si no se toco o se fue por el stop
+def crea_target_long(df, nombre_target = "LONG_target_20p_05", porciento_varia = 0.5, proyeccion = 20):
+  for indice, fila in df.iterrows():
+    indice_inicio = df.index.get_loc(indice)#Obtiene el indice en el cual se esta iterando
+    df.loc[indice,nombre_target]=toca_long(referencia = df.loc[indice,'close'], porciento_varia = porciento_varia, columna = df['close'].iloc[indice_inicio:],proyeccion=proyeccion)
+
+  df['tomorrow'] = df["close"].shift(-1)
+  df['target'] = (df['tomorrow'] > df["close"]).astype(int)
+
+  return df
